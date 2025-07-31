10c4ff9ddd58442d3c6e193b91df9c2ab4e3eb83
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2025-07-31 16:19:07 +0200

    Add basic game state with useState, and CellComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-
+import { useState } from "react"
+import CellComponent from "./components/CellComponent"
 
 const App = () => {
+
+  const [cells, setCells] = useState(Array(9).fill(""));
+  const [whoPlay, setWhoPlay] = useState("cross");
+  const [winner, setWinner] = useState(null);
+
   return (
     <div>
       <h1>Tic Tac Toe</h1>
+
+      <div className="gameContainer">
+        {cells.map((cell, index) => {
+          return <CellComponent key={index} />
+        })}
+      </div>
     </div>
   )
 }
---
+++ src/components/CellComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CellComponent = () => {
+  return (
+    <div>
+      Cell
+    </div>
+  )
+}
+
+export default CellComponent
