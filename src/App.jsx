7e0eb08b5cfa65e9da5d59e4aa7f400f819f28f1
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2025-07-31 19:46:26 +0200

    Implement checkWinner to detect winner or draw

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,46 @@ import CellComponent from "./components/CellComponent"
 
 const App = () => {
 
+  // states
   const [cells, setCells] = useState(Array(9).fill(""));
   const [whoPlay, setWhoPlay] = useState("cross");
   const [winner, setWinner] = useState(null);
 
   // functions
   const handleClick = (id) => {
-      if(cells[id] !=="" || winner) return
+    if (cells[id] !== "" || winner) return
 
-      const updatedCells = [...cells]
-      updatedCells[id] = whoPlay;
+    const updatedCells = [...cells]
+    updatedCells[id] = whoPlay;
 
-      setCells(updatedCells)      
-      setWhoPlay(whoPlay === "cross" ? "circle" : "cross");
-      
+    setCells(updatedCells)
+    setWhoPlay(whoPlay === "cross" ? "circle" : "cross");
+
+    checkWinner(updatedCells)
+  }
+
+  const checkWinner = (currentCell) => {
+    const winningCombos = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], //horizontal
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], //vertical
+      [0, 4, 8], [2, 4, 6]             //diagonal
+    ];
+
+    for (let combo of winningCombos) {
+      const [a, b, c] = combo;
+      // chacking winner
+      if (currentCell[a] && currentCell[a] === currentCell[b] && currentCell[b] === currentCell[c]) {
+        setWinner(currentCell[a]);
+        console.log(`Winner is ${currentCell[a]}`);
+        return;
+      }
+    }
+
+    // checking draw
+    if (!currentCell.includes("")) {
+      setWinner("draw");
+      console.log("It is Draw");
+    }
   }
 
   return (
