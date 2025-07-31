9e8e4061a7e0564db29bcb6dfc82adfa8ff2a11f
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2025-07-31 17:53:46 +0200

    Add handleClick logic for cell update and switching player

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+// components
 import CellComponent from "./components/CellComponent"
 
 const App = () => {
@@ -7,13 +8,25 @@ const App = () => {
   const [whoPlay, setWhoPlay] = useState("cross");
   const [winner, setWinner] = useState(null);
 
+  // functions
+  const handleClick = (id) => {
+      if(cells[id] !=="" || winner) return
+
+      const updatedCells = [...cells]
+      updatedCells[id] = whoPlay;
+
+      setCells(updatedCells)      
+      setWhoPlay(whoPlay === "cross" ? "circle" : "cross");
+      
+  }
+
   return (
     <div className="app">
       <h1>Tic Tac Toe</h1>
 
       <div className="gameContainer">
         {cells.map((cell, index) => {
-          return <CellComponent key={index} />
+          return <CellComponent key={index} cell={cell} id={index} handleClick={handleClick} />
         })}
       </div>
     </div>
--- src/components/CellComponent.jsx
+++ src/components/CellComponent.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const CellComponent = () => {
+const CellComponent = (props) => {
+
+  const {cell, id, handleClick} = props
+
   return (
-    <div className='gameCell'>
-      Cell
+    <div className='gameCell' id={id} onClick={() => handleClick(id)} >
+      <div className={cell}></div>
     </div>
   )
 }
